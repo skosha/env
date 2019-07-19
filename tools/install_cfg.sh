3d1275fa3f781cbf13eb515febe08d1acf6f7da8
Kosha Shah <kosha.shah087@gmail.com>
2019-07-19 14:46:34 +0100

    [Incomplete] Clean-up install script

--- tools/install_cfg.sh
+++ tools/install_cfg.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 TEMP_DIR=$HOME/config_tmp
+ENV_DIR=$HOME/env
+
+if [ -d '$ENV_DIR' ]; then
+    git clone --recursive https://github.com/skosha/env.git $HOME/env
+fi
 
 # Unzip the files
 if [ -f $1 ]; then
@@ -21,9 +26,6 @@ if [ -f $1 ]; then
     cd $HOME
 fi
 
-CONFIG_DIR=$HOME/.dot_config
-[ -d "$CONFIG_DIR" ] || cp -a $TEMP_DIR/.dot_config $HOME/
-
 # Get this distro
 DISTRO=$( cat /etc/*-release | tr [:upper:] [:lower:] | grep -Poi '(debian|ubuntu|red hat|centos)' | uniq )
 if [ -z $DISTRO ]; then
@@ -31,49 +33,64 @@ if [ -z $DISTRO ]; then
 fi
 echo "Detected Linux distribution: $DISTRO"
 
+# Get bin files from the env git repo
+[ -d $HOME/bin ] || mkdir ~/bin
+cp -af $ENV_DIR/bin/* $HOME/bin/
+[ -f $HOME/bin/fzf ] || chmod +x $HOME/bin/fzf
+
+[ -d $HOME/dot-files ] || mkdir $HOME/dot-files
+
+ln -s $ENV_DIR/dot-files/aliasrc $HOME/.dot-files/.aliasrc
+ln -s $ENV_DIR/dot-files/mostrc $HOME/.dot-files/.mostrc
+ln -s $ENV_DIR/dot-files/gitignore $HOME/.gitignore
+ln -s $ENV_DIR/dot-files/screenrc $HOME/.screenrc
+ln -s $ENV_DIR/dot-files/zshrc $HOME/.zshrc
+
 # Install fzf
-[ -f $HOME/bin/fzf ] ||
-if [ -f $TEMP_DIR/fzf] ; then
-    cp $TEMP_DIR/fzf $HOME/bin/
-    chmod +x $HOME/bin/fzf
-    cp -a $TEMP_DIR/.fzf $HOME/
-    source $HOME/.fzf/install
-else
-    echo 'No fzf file'
-fi
+[ -d $HOME/.fzf ] || mkdir $HOME/.fzf
+cp -af $ENV_DIR/fzf/* $HOME/.fzf/
+source $HOME/.fzf/install
+ln -s $HOME/env/dot-files/fzf.bash $HOME/.fzf.bash
 
 # Install tmux
 command -v tmux > /dev/null 2>&1 ||
 if [ "$DISTRO" == "ubuntu" ] ; then
     sudo apt-get install tmux
+    [ -d $HOME/.tmux ] || mkdir -p $HOME/.tmux
+    ln -s $ENV_DIR/dot-files/tmux.conf $HOME/.tmux.conf
+    ln -s $ENV_DIR/dot-files/tmux-linux.conf $HOME/.tmux-linux.conf
+    ln -s $ENV_DIR/dot-files/tmux-osx.conf $HOME/.tmux-osx.conf
+
+    # Install tmuxp
+    command -v tmuxp > /dev/null 2>&1 ||
+    pip install --user tmuxp
+    [ -d $HOME/.tmuxp ] || mkdir -p $HOME/.tmuxp
+    cp -af $HOME/env/tmuxp/* $HOME/.tmuxp/
+
+    # Install xsel, used by tmux-yank
+    if [ "$DISTRO" == "ubuntu" ] ; then
+        sudo apt-get install xsel
+    fi
+
+    # Tmux plugins and configs
+    mkdir -p $HOME/.tmux
+    cp -a $TEMP_DIR/.tmux/* $HOME/.tmux/*
+    mkdir -p $HOME/tools/tmux_plugins
+    cp -a $TEMP_DIR/tmux_plugins/* $HOME/tools/tmux_plugins/
 fi
 
-# Install tmuxp
-command -v tmuxp > /dev/null 2>&1 ||
-pip install --user tmuxp
-cp -a $TEMP_DIR/.tmuxp $HOME/
-
-# Install xsel, used by tmux-yank
-if [ "$DISTRO" == "ubuntu" ] ; then
-    sudo apt-get install xsel
-fi
-
-# Tmux plugins and configs
-mkdir -p $HOME/.tmux
-cp -a $TEMP_DIR/.tmux/* $HOME/.tmux/*
-mkdir -p $HOME/tools/tmux_plugins
-cp -a $TEMP_DIR/tmux_plugins/* $HOME/tools/tmux_plugins/
-cp $TEMP_DIR/.tmux.conf $HOME/
-
-# Install screenrc
-cp $TEMP_DIR/.screenrc $HOME/
-
-# Install .customrc
-echo '[ -f '$CONFIG_DIR'/.customrc ] && source '$CONFIG_DIR'/.customrc' >> $HOME/.bashrc
-echo '[ -f '$CONFIG_DIR'/.customrc ] && source '$CONFIG_DIR'/.customrc' >> $HOME/.bash_profile
+echo $HOME/env/dot-files/bashrc_cat >> $HOME/.bashrc
 
 # Install vim config
 mkdir -p $HOME/.vim
+mkdir -p $HOME/.vim/temp_dirs/undodir
+mkdir -p $HOME/.vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+ln -s $ENV_DIR/dot-files/vimrc $HOME/.vimrc
+vim +PluginInstall +qall
+mkdir -p $HOME/.vim/plugin
+ln -s $ENV_DIR/vim/plugin/* $HOME/.vim/plugin/*
+cp -s $ENV_DIR/colors $HOME/.vim/
 cp -a $TEMP_DIR/.vim/* $HOME/.vim/
 
 # Install local
@@ -92,7 +109,7 @@ if [ -f $TEMP_DIR/synergy-*.deb ] ; then
 fi
 
 # Gitignore
-[ -f $CONFIG_DIR/.gitignore ] && git config --global core.excludesfile $CONFIG/.gitignore
+[ -f $DOT_DIR/.gitignore ] && git config --global core.excludesfile $CONFIG/.gitignore
 
 # At the end, delete the temp directory
 rm -rf $TEMP_DIR > /dev/null
